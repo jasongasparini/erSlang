5e8910f386c5e6806571db9a7bd3f1824417c54a
jasongasparini <123187923+jasongasparini@users.noreply.github.com>
2023-11-22 15:41:03 +0000

    Added utils for server moves

--- lab 4/tttServer.erl
+++ lab 4/tttServer.erl
@@ -45,6 +45,8 @@ serverLoop() -> receive
                       io:fwrite("~sReceived [process_player_turn] request from node ~w with board ~w and player move ~w.~n",[?id, FromNode, Board, PlayerPos]),
                       NewBoard = processPlayerMove(PlayerPos, Board),
                       % Do more stuff here.
+
+                      {tttServer, self()} ! {}
                       serverLoop();
 
                    {FromNode, computer_turn, Board} ->
@@ -76,3 +78,23 @@ replaceInList(Value, Position, List) ->
    {Part1, Part2} = lists:split(Position-1, List),     % Break the list in two just before the specified Position.
    [_ | Tail] = Part2,                                 % Separate Part2 into Head and Tail, discarding the Head.
    Part1 ++ [Value] ++ Tail.                           % CONS together the result: Part1 ++ the new Value ++ the Tail from Part2.
+
+makeMove(Board) ->
+   ComputerMove = calculate(0, Board),
+   io:fwrite("Server is Placing a O into position ~w.~n", [ComputerMove]),
+    UpdatedBoard = replaceInList(-1, ComputerMove, Board),
+    UpdatedBoard.
+
+openPos(Target, [Head | Tail]) when (Target == Head) -> 1;
+openPos(Target, [Head | Tail]) when (Target /= Head) -> 1 + openPos(Target, Tail);      
+openPos(Target, [])                                  -> 1.
+
+winner(Board) ->
+   Patterns = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]],
+   lists:any(fun(Condition) -> checkForPattern(Board, Condition) end, Patterns).
+
+checkForPattern(Board, [A, B, C]) ->
+    ElementA = lists:nth(A, Board),
+    ElementB = lists:nth(B, Board),
+    ElementC = lists:nth(C, Board),
+    (ElementA =:= ElementB) andalso (ElementB =:= ElementC) andalso (ElementA =/= 0).
